0b9995b8fcce11562c629b3b113258f82c84e76a
cocktailpeanut <cocktailpeanuts@proton.me>
2024-07-11 06:54:05 -0400

    update

--- pinokio.js
+++ pinokio.js
@@ -10,6 +10,7 @@ module.exports = {
     let running = await kernel.running(__dirname, "start.js")
     if (installing) {
       return [{
+        default: true,
         icon: "fa-solid fa-plug",
         text: "Installing",
         href: "install.js",
@@ -19,6 +20,7 @@ module.exports = {
         let local = kernel.memory.local[path.resolve(__dirname, "start.js")]
         if (local && local.url) {
           return [{
+            default: true,
             icon: "fa-solid fa-rocket",
             text: "Open Web UI",
             href: local.url,
@@ -29,6 +31,7 @@ module.exports = {
           }]
         } else {
           return [{
+            default: true,
             icon: 'fa-solid fa-terminal',
             text: "Terminal",
             href: "start.js",
@@ -36,6 +39,7 @@ module.exports = {
         }
       } else {
         return [{
+          default: true,
           icon: "fa-solid fa-power-off",
           text: "Start",
           href: "start.js",
@@ -55,6 +59,7 @@ module.exports = {
       }
     } else {
       return [{
+        default: true,
         icon: "fa-solid fa-plug",
         text: "Install",
         href: "install.js",
